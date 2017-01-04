51678b338a8d1a195df40c1ec4ebe1ee7242e98f
parrt <parrt@cs.usfca.edu>
2017-01-04 10:05:12 -0800

    add scaffolding

--- src/us/parr/rf/DecisionTree.java
+++ src/us/parr/rf/DecisionTree.java
@@ -1,4 +1,11 @@
 package us.parr.rf;
 
 public class DecisionTree {
+	protected int splitVariable;
+	protected int splitValue;
+
+	public DecisionTree(int splitVariable, int splitValue) {
+		this.splitVariable = splitVariable;
+		this.splitValue = splitValue;
+	}
 }
--- src/us/parr/rf/RandomForest.java
+++ src/us/parr/rf/RandomForest.java
@@ -1,7 +1,37 @@
 package us.parr.rf;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomForest {
+	/** Number of samples */
+	protected int N;
+
+	/** Number of variables to select at random at each decision node to find best split */
+	protected int m;
+
+	/** How many trees to create in the forest */
+	protected int numEstimators;
+
+	/** The forest of trees */
 	protected List<DecisionTree> trees;
+
+	public RandomForest(int numEstimators) {
+		this.numEstimators = numEstimators;
+		this.trees = new ArrayList<>(numEstimators);
+	}
+
+	/** Train numEstimators trees using 2D matrix X -> vector Y as training data,
+	 *  where Y_i is the category of the ith feature vector X_i.
+	 */
+	public void train(List<int[]> X, List<Integer> Y) {
+		if ( X==null || X.size()==0 ) return;
+		int[] first = X.get(0);
+		N = X.size();
+		this.m = (int)Math.sqrt(first.length);
+	}
+
+	public int classify(int[] unknown) {
+		return 0;
+	}
 }
