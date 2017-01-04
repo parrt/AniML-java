2ea480daf310a8b81bfdeb068e56a5b6ebc6a04c
parrt <parrt@cs.usfca.edu>
2017-01-04 12:55:15 -0800

    don't need leaf node

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Simple Random Forest in Java
+# Simple Random Forest(tm) in Java
 
 [codebuff](https://github.com/antlr/codebuff) could really use a random forest so I'm playing with an implementation here.
 
@@ -9,3 +9,5 @@ All `int` values but supports categorical and numerical values.
 **References**
 
 [ironmanMA](https://github.com/ironmanMA/Random-Forest) has some nice notes.
+
+[Breiman's RF site](https://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm)
--- src/us/parr/rf/DecisionTree.java
+++ src/us/parr/rf/DecisionTree.java
@@ -1,11 +1,33 @@
 package us.parr.rf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static us.parr.rf.RandomForest.INVALID_CATEGORY;
+
 public class DecisionTree {
+	/** This node is split on which variable? */
 	protected int splitVariable;
+
+	/** Split at what variable value? */
 	protected int splitValue;
+
+	/** List of child nodes if not a leaf node; non-null implies not a leaf node. */
+	protected List<DecisionTree> children;
+
+	/** The predicted category if this is a leaf node; non-leaf by default */
+	protected int category = INVALID_CATEGORY;
+
+	public DecisionTree() {
+	}
 
 	public DecisionTree(int splitVariable, int splitValue) {
 		this.splitVariable = splitVariable;
 		this.splitValue = splitValue;
+		this.children = new ArrayList<>();
 	}
+
+	public boolean isLeaf() { return children==null || category==INVALID_CATEGORY; }
+
+	public void makeLeaf() { children=null; category=INVALID_CATEGORY; }
 }
